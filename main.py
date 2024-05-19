33c05163eb473f12a5a77b378b1da0472db10efa
JSF Studio <164411207+jsfstudio@users.noreply.github.com>
2024-05-20 01:02:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     """
     st.title("JSF Summarizer")
 
-    input_method = st.radio("Select input method", ('Upload a document', 'Enter a YouTube URL'))
+    input_method = st.radio("Select input method", ('Upload a document'))
 
     if input_method == 'Upload a document':
         uploaded_file = st.file_uploader("Upload a document to summarize, 10k to 100k tokens works best!", type=['txt', 'pdf'])
@@ -28,11 +28,11 @@ def main():
         youtube_url = st.text_input("Enter a YouTube URL to summarize")
 
     api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
-    st.markdown('[Author email](mailto:ethanujohnston@gmail.com)')
+    st.markdown('[Author email](mailto:connect@jsfstudio.co)')
     use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
-    find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
+    #find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
     st.sidebar.markdown('# Made by: [JSF Studio]')
-    st.sidebar.markdown('# Git link: [Docsummarizer]')
+    st.sidebar.markdown('# Git link: [Caption Generator]')
     st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
                         This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
 
